7302d1122b85b96fe160dfc74abf9f11cce27d86
alves-Moises <moisesinho_ra@hotmail.com>
2022-10-26 09:49:11 -0300

    update time controller: fix errors

--- controller/TimeController.py
+++ controller/TimeController.py
@@ -1,19 +1,20 @@
 from sqlite3 import Time
-from time import time
 import controller.JogadorController
 from controller.JogadorController import JogadorController
 from controller.OrcamentoController import OrcamentoController
 from view.TimeView import TimeView
+
 class TimeController():
     def __init__(self):
         # self.Jogadores = JogadorController()
         self.time = Time
         self.timeview = TimeView()
-        pass
+        self.jogadores = JogadorController()
         
     def opcoes(self):  #opcoes jogador
+        print("Opcoes jogador")
         self.__escolhas = {
-            "1": JogadorController.listar_jogadores, 
+            "1": self.jogadores.listar_jogadores, 
             "2": OrcamentoController.ver_saldo,
             "3": OrcamentoController.ver_saldo,
             "4": "ver gastos"
@@ -22,12 +23,13 @@ class TimeController():
         # pega escolha e executar o comando
         self.__escolha = self.timeview.opcoes()
         if self.__escolha in self.__escolhas:
-            self.__escolhas[self.__escolha](self)
+            self.__escolhas[self.__escolha]()
 
         # if self.__escolha == '3':
         #     OrcamentoController.ver_saldo(self, Time)
 
     def menu_principal(self):  #menu inicial
+        print("Menu principal")
         self.__escolhas = {
             "1": JogadorController.tela_opcoes,  #jogador opcoes
             "2": TimeController.opcoes   #time opcoes
