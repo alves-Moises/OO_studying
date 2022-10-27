a278103ba59cc37da4fe15300fb8f95ce8e08705
alves-Moises <moisesinho_ra@hotmail.com>
2022-10-26 22:44:22 -0300

    refactor timeCOntroler

--- controller/TimeController.py
+++ controller/TimeController.py
@@ -3,6 +3,7 @@ import controller.JogadorController
 from controller.JogadorController import JogadorController
 from controller.OrcamentoController import OrcamentoController
 from view.TimeView import TimeView
+from view.JogadorView import JogadorView
 
 class TimeController():
     def __init__(self):
@@ -10,12 +11,12 @@ class TimeController():
         self.time = Time
         self.timeview = TimeView()
         self.jogadores = JogadorController()
-        
+        self.viewjogador = JogadorView()
     def opcoes(self):  #opcoes jogador
         print("Opcoes jogador")
         self.__escolhas = {
             "1": self.jogadores.listar_jogadores, 
-            "2": OrcamentoController.ver_saldo,
+            "2": OrcamentoController.ver_or,
             "3": OrcamentoController.ver_saldo,
             "4": "ver gastos"
             }
@@ -44,5 +45,25 @@ class TimeController():
                
             
 
+    def tela_opcoes(self):
+        print('asd')
+        self.__escolhas = {
+            "1": self.viewjogador.listar_jogadores,
+            "2": "exibir_jogador especifico",
+            "3": self.viewjogador.adicionar_jogador,
+            "4": "remover_jogador"
+        }
+        escolha = self.viewjogador.tela_opcoes(self)
+        if escolha == '1':
+            JogadorController.listar_jogadores(self)
 
+        if escolha == '2':
+            pass
+
+        if escolha == '3':
+            JogadorController.jogadores =  JogadorController.adicionar_jogador(self)
+        print('sucesso')
+        self.__escolhas[self.__escolha](self)
+
+    
        
